ad48ad6d73ad73e377396dea793b1756f5b6274c
LuuHongNhung <nhungluu204@gmail.com>
2024-12-05 18:00:42 +0700

    YourfavouriteActivity.java activity_yourfavourite.xml

--- app/src/main/java/com/example/phanmembansach/YourfavouriteActivity.java
+++ app/src/main/java/com/example/phanmembansach/YourfavouriteActivity.java
@@ -6,30 +6,87 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class YourfavouriteActivity extends AppCompatActivity {
     private ImageView back;
-    private TextView tvoldprice;
-    private TextView tvoldprice2;
-    ImageView img_cart_1;
-    ImageView img_cart_2;
-    LinearLayout a;
-    LinearLayout b;
+    ListView lv;
+    Adapter_Categories_books adapterCategoriesBooks ;
+
+
+    ArrayList<Book> arrbook = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DatabaseReference mdata = FirebaseDatabase.getInstance().getReference();
         setContentView(R.layout.activity_yourfavourite);
-        tvoldprice = findViewById(R.id.giacu);
-        tvoldprice2 =findViewById(R.id.tvoldprice2);
-        tvoldprice.setPaintFlags(tvoldprice.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-        tvoldprice2.setPaintFlags(tvoldprice.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
-        img_cart_1 = findViewById(R.id.ic_cart_1);
-        img_cart_2 = findViewById(R.id.ic_cart_2);
-        a = findViewById(R.id.a);
-        b = findViewById(R.id.b);
+        lv = findViewById(R.id.lv);
+        App app = (App) getApplicationContext();
+        adapterCategoriesBooks = new Adapter_Categories_books(this,R.layout.row_categories_books,arrbook);
+        lv.setAdapter(adapterCategoriesBooks);
+
+        mdata.child("SachYeuThichs").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                arrbook.clear();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren())
+                {
+                    try
+                    {
+                        SachYeuThich sachYeuThich = dataSnapshot.getValue(SachYeuThich.class);
+                        if (app.getUsername().equals(sachYeuThich.getTenDangNhap()))
+                        {
+                            mdata.child("Sachs").addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    for(DataSnapshot dataSnapshot : snapshot.getChildren())
+                                    {
+                                        try {
+                                            Book book = dataSnapshot.getValue(Book.class);
+                                            if(book.getSachID()== sachYeuThich.getSachID())
+                                            {
+
+                                                arrbook.add(book);
+                                            }
+                                        }catch (Exception e)
+                                        {
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+                        }
+
+                    }catch (Exception e)
+                    {
+
+                    }
+                }
+                adapterCategoriesBooks.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
         back = findViewById(R.id.img_back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,33 +96,6 @@ public class YourfavouriteActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        img_cart_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(YourfavouriteActivity.this, Home.class);
-                intent.putExtra("fragment_cart", 2);
-                startActivity(intent);
-            }
-        });
-        img_cart_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(YourfavouriteActivity.this, Home.class);
-                intent.putExtra("fragment_cart", 2);
-                startActivity(intent);
-            }
-        });
-        a.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(YourfavouriteActivity.this, Detail_book.class));
-            }
-        });
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(YourfavouriteActivity.this, Detail_book.class));
-            }
-        });
+
     }
 }
